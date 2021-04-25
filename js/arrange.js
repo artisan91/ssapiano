f48588ba179f7375bb4efd47968a1bbacc63db93
Youngjin <ojin0611@gmail.com>
2021-04-25 16:36:17 +0900

    design one line

---
+++ js/arrange.js
@@ -0,0 +1,297 @@
+const oneLine = `
+<div class="keys">
+  <!--
+  <div data-key="Shift" data-code="F2" class="octave1 key">
+    <span class="keyboard">Shift.L</span>
+  </div>
+  <div data-key="a" data-code="F#2" class="octave1 key sharp">
+    <span class="keyboard">A</span>
+  </div>
+  <div data-key="z" data-code="G2" class="octave1 key">
+    <span class="keyboard">Z</span>
+  </div>
+  <div data-key="s" data-code="G#2" class="octave1 key sharp">
+    <span class="keyboard">S</span>
+  </div>
+  -->
+  <div data-key="x" data-code="A2" class="octave1 key">
+    <span class="keyboard">X</span>
+  </div>
+  <div data-key="d" data-code="A#2" class="octave1 key sharp">
+    <span class="keyboard">D</span>
+  </div>
+  <div data-key="c" data-code="B2" class="octave1 key">
+    <span class="keyboard">C</span>
+  </div>
+  <div data-key="v" data-code="C3" class="octave2 key">
+    <span class="keyboard">V</span>
+  </div>
+  <div data-key="g" data-code="C#3" class="octave2 key sharp"> 
+    <span class="keyboard">G</span>
+  </div>
+  <div data-key="b" data-code="D3" class="octave2 key">
+    <span class="keyboard">B</span>
+  </div>
+  <div data-key="h" data-code="D#3" class="octave2 key sharp">
+    <span class="keyboard">H</span>
+  </div>
+  <div data-key="n" data-code="E3" class="octave2 key">
+    <span class="keyboard">N</span>
+  </div>
+  <div data-key="m" data-code="F3" class="octave2 key">
+    <span class="keyboard">M</span>
+  </div>
+  <div data-key="k" data-code="F#3" class="octave2 key sharp">
+    <span class="keyboard">K</span>
+  </div>
+  <div data-key="," data-code="G3" class="octave2 key">
+    <span class="keyboard">,</span>
+  </div>
+  <div data-key="l" data-code="G#3" class="octave2 key sharp">
+    <span class="keyboard">L</span>
+  </div>
+  <div data-key="." data-code="A3" class="octave2 key">
+    <span class="keyboard">.</span>
+  </div>
+  <div data-key=";" data-code="A#3" class="octave2 key sharp">
+    <span class="keyboard">;</span>
+  </div>
+  <div data-key="/" data-code="B3" class="octave2 key">
+    <span class="keyboard">/</span>
+  </div>
+  <div data-key="q" data-code="C4" class="octave3 key">
+    <span class="keyboard">Q</span>
+  </div>
+  <div data-key="2" data-code="C#4" class="octave3 key sharp">
+    <span class="keyboard">2</span>
+  </div>
+  <div data-key="w" data-code="D4" class="octave3 key">
+    <span class="keyboard">W</span>
+  </div>
+  <div data-key="3" data-code="D#4" class="octave3 key sharp">
+    <span class="keyboard">3</span>
+  </div>
+  <div data-key="e" data-code="E4" class="octave3 key">
+    <span class="keyboard">E</span>
+  </div>
+  <div data-key="r" data-code="F4" class="octave3 key">
+    <span class="keyboard">R</span>
+  </div>
+  <div data-key="5" data-code="F#4" class="octave3 key sharp">
+    <span class="keyboard">5</span>
+  </div>
+  <div data-key="t" data-code="G4" class="octave3 key">
+    <span class="keyboard">T</span>
+  </div>
+  <div data-key="6" data-code="G#4" class="octave3 key sharp">
+    <span class="keyboard">6</span>
+  </div>
+  <div data-key="y" data-code="A4" class="octave3 key">
+    <span class="keyboard">Y</span>
+  </div>
+  <div data-key="7" data-code="A#4" class="octave3 key sharp">
+    <span class="keyboard">7</span>
+  </div>
+  <div data-key="u" data-code="B4" class="octave3 key">
+    <span class="keyboard">U</span>
+  </div>
+  <div data-key="i" data-code="C5" class="octave4 key">
+    <span class="keyboard">I</span>
+  </div>
+  <div data-key="9" data-code="C#5" class="octave4 key sharp">
+    <span class="keyboard">9</span>
+  </div>
+  <div data-key="o" data-code="D5" class="octave4 key">
+    <span class="keyboard">O</span>
+  </div>
+  <div data-key="0" data-code="D#5" class="octave4 key sharp">
+    <span class="keyboard">0</span>
+  </div>
+  <div data-key="p" data-code="E5" class="octave4 key">
+    <span class="keyboard">P</span>
+  </div>
+  <div data-key="[" data-code="F5" class="octave4 key">
+    <span class="keyboard">[</span>
+  </div>
+  <div data-key="=" data-code="F#5" class="octave4 key sharp">
+    <span class="keyboard">=</span>
+  </div>
+  <div data-key="]" data-code="G5" class="octave4 key">
+    <span class="keyboard">]</span>
+  </div>
+
+</div>
+`
+const twoLines = 
+`
+<div class="keys">
+  <div data-key="q" data-code="C4" class="octave3 key">
+    <span class="keyboard">Q</span>
+  </div>
+  <div data-key="2" data-code="C#4" class="octave3 key sharp">
+    <span class="keyboard">2</span>
+  </div>
+  <div data-key="w" data-code="D4" class="octave3 key">
+    <span class="keyboard">W</span>
+  </div>
+  <div data-key="3" data-code="D#4" class="octave3 key sharp">
+    <span class="keyboard">3</span>
+  </div>
+  <div data-key="e" data-code="E4" class="octave3 key">
+    <span class="keyboard">E</span>
+  </div>
+  <div data-key="r" data-code="F4" class="octave3 key">
+    <span class="keyboard">R</span>
+  </div>
+  <div data-key="5" data-code="F#4" class="octave3 key sharp">
+    <span class="keyboard">5</span>
+  </div>
+  <div data-key="t" data-code="G4" class="octave3 key">
+    <span class="keyboard">T</span>
+  </div>
+  <div data-key="6" data-code="G#4" class="octave3 key sharp">
+    <span class="keyboard">6</span>
+  </div>
+  <div data-key="y" data-code="A4" class="octave3 key">
+    <span class="keyboard">Y</span>
+  </div>
+  <div data-key="7" data-code="A#4" class="octave3 key sharp">
+    <span class="keyboard">7</span>
+  </div>
+  <div data-key="u" data-code="B4" class="octave3 key">
+    <span class="keyboard">U</span>
+  </div>
+  <div data-key="i" data-code="C5" class="octave4 key">
+    <span class="keyboard">I</span>
+  </div>
+  <div data-key="9" data-code="C#5" class="octave4 key sharp">
+    <span class="keyboard">9</span>
+  </div>
+  <div data-key="o" data-code="D5" class="octave4 key">
+    <span class="keyboard">O</span>
+  </div>
+  <div data-key="0" data-code="D#5" class="octave4 key sharp">
+    <span class="keyboard">0</span>
+  </div>
+  <div data-key="p" data-code="E5" class="octave4 key">
+    <span class="keyboard">P</span>
+  </div>
+  <div data-key="[" data-code="F5" class="octave4 key">
+    <span class="keyboard">[</span>
+  </div>
+  <div data-key="=" data-code="F#5" class="octave4 key sharp">
+    <span class="keyboard">=</span>
+  </div>
+  <div data-key="]" data-code="G5" class="octave4 key">
+    <span class="keyboard">]</span>
+  </div>
+</div>
+<div class="keys">
+  <!--
+  <div data-key="Shift" data-code="F2" class="octave1 key">
+    <span class="keyboard">Shift.L</span>
+  </div>
+  <div data-key="a" data-code="F#2" class="octave1 key sharp">
+    <span class="keyboard">A</span>
+  </div>
+  -->
+  <div data-key="z" data-code="G2" class="octave1 key">
+    <span class="keyboard">Z</span>
+  </div>
+  <div data-key="s" data-code="G#2" class="octave1 key sharp">
+    <span class="keyboard">S</span>
+  </div>
+  <div data-key="x" data-code="A2" class="octave1 key">
+    <span class="keyboard">X</span>
+  </div>
+  <div data-key="d" data-code="A#2" class="octave1 key sharp">
+    <span class="keyboard">D</span>
+  </div>
+  <div data-key="c" data-code="B2" class="octave1 key">
+    <span class="keyboard">C</span>
+  </div>
+  <div data-key="v" data-code="C3" class="octave2 key">
+    <span class="keyboard">V</span>
+  </div>
+  <div data-key="g" data-code="C#3" class="octave2 key sharp"> 
+    <span class="keyboard">G</span>
+  </div>
+  <div data-key="b" data-code="D3" class="octave2 key">
+    <span class="keyboard">B</span>
+  </div>
+  <div data-key="h" data-code="D#3" class="octave2 key sharp">
+    <span class="keyboard">H</span>
+  </div>
+  <div data-key="n" data-code="E3" class="octave2 key">
+    <span class="keyboard">N</span>
+  </div>
+  <div data-key="m" data-code="F3" class="octave2 key">
+    <span class="keyboard">M</span>
+  </div>
+  <div data-key="k" data-code="F#3" class="octave2 key sharp">
+    <span class="keyboard">K</span>
+  </div>
+  <div data-key="," data-code="G3" class="octave2 key">
+    <span class="keyboard">,</span>
+  </div>
+  <div data-key="l" data-code="G#3" class="octave2 key sharp">
+    <span class="keyboard">L</span>
+  </div>
+  <div data-key="." data-code="A3" class="octave2 key">
+    <span class="keyboard">.</span>
+  </div>
+  <div data-key=";" data-code="A#3" class="octave2 key sharp">
+    <span class="keyboard">;</span>
+  </div>
+  <div data-key="/" data-code="B3" class="octave2 key">
+    <span class="keyboard">/</span>
+  </div>
+</div>
+`
+// key 배열 조정
+const Keys = document.getElementById("main");
+const oneLineBtn = document.getElementById("oneLineBtn");
+const twoLinesBtn = document.getElementById("twoLinesBtn");
+let sharpKeys;
+let notSharpKeys;
+
+// 한 줄
+oneLineBtn.addEventListener('click', 
+  function(){
+    console.log("one");
+    Keys.innerHTML = oneLine;
+
+    sharpKeys = document.querySelectorAll('.key.sharp'); // 흑건
+    sharpKeys.forEach((sharpKey) => {
+      sharpKey.style.width = "2.6%";
+    })
+
+    notSharpKeys = document.querySelectorAll('.key:not(.sharp)'); // 백건
+    notSharpKeys.forEach((notSharpKey) => {
+      notSharpKey.style.width="4%";
+      notSharpKey.style.left ="1.3%";
+    })
+  },
+  false
+) 
+
+// 두 줄
+twoLinesBtn.addEventListener('click', 
+function(){
+  console.log("one");
+  Keys.innerHTML = twoLines;
+
+  sharpKeys = document.querySelectorAll('.key.sharp'); // 흑건
+  sharpKeys.forEach((sharpKey) => {
+    sharpKey.style.width = "4%";
+  })
+
+  notSharpKeys = document.querySelectorAll('.key:not(.sharp)'); // 백건
+  notSharpKeys.forEach((notSharpKey) => {
+    notSharpKey.style.width="7%";
+    notSharpKey.style.left ="2%";
+  })
+},
+false
+) 
+
